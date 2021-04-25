5d9c6d50f4eabdc396075fbd514e4800d2178dc9
Comatoznik <58427696+Comatoznik@users.noreply.github.com>
2021-04-25 18:31:57 +0300

    Add Code

---
+++ CheatSheet/MainPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+namespace CheatSheet
+{
+    public class Commi
+    {
+        public string Surname { get; set; }
+        public string Years { get; set; }
+    }
+    public partial class MainPage : ContentPage
+    {
+        public List<Commi> Commies { get; set; }
+        public MainPage()
+        {
+            InitializeComponent();
+            Commies = new List<Commi>
+            {
+                new Commi {Surname ="Ленин", Years = "1917-1922"},
+                   new Commi {Surname ="Сталин", Years = "1922-1953"},
+                      new Commi {Surname ="Хрущев", Years = "1953-1964"},
+                         new Commi {Surname ="Брежнев", Years = "1964-1982"},
+                            new Commi {Surname ="Горбачев", Years = "1982-1991"}
+            };
+            this.BindingContext = this;
+          
+        }
+
+        private void CommiesList_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {   
+           
+                Page1 page1 = new Page1();
+                Navigation.PushAsync(page1);
+            page1.content(e.SelectedItemIndex);
+               
+          
+        }
+    }
+}
