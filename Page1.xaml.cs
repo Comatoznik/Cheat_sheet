44727899e2c5c6529d2b4098d9ce43a1a1e253a0
Comatoznik <58427696+Comatoznik@users.noreply.github.com>
2021-04-25 18:57:43 +0300

    Add files via upload

---
+++ Page1.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CheatSheet
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Page1 : ContentPage
+    {
+        public Page1()
+        {
+            InitializeComponent();
+         
+          
+
+        }
+        public void content(int temp)
+        {
+            switch (temp)
+            {
+                case 0:
+                    one.IsVisible = true;
+                    break;
+                case 1:
+                    two.IsVisible = true;
+                    break;
+                case 2:
+                    three.IsVisible = true;
+                    break;
+                case 3:
+                    four.IsVisible = true;
+                    break;
+                case 4:
+                    five.IsVisible = true;
+                    break;
+            }   
+        }
+    }
+}
